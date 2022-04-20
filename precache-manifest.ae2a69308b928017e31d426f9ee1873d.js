eb09a63defcd62dde3ee3e96025edc67f4c07875
Pavel Antonov <freislot@gmail.com>
2022-04-20 04:47:09 +0300

    Updates

--- precache-manifest.ae2a69308b928017e31d426f9ee1873d.js
+++ precache-manifest.ae2a69308b928017e31d426f9ee1873d.js
@@ -1,23 +1,23 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "f9889202444ffb56785ad4456cac8fdd",
+    "revision": "a00390aeeb46d85e10c1db4172e8dfbe",
     "url": "/react-fancy-timeline/index.html"
   },
   {
-    "revision": "c706e1f83e6f64242e82",
+    "revision": "d3e04443ea38635c93b1",
     "url": "/react-fancy-timeline/static/css/main.c8ad5d64.chunk.css"
   },
   {
-    "revision": "b4d939ba8b3b184181c4",
-    "url": "/react-fancy-timeline/static/js/2.f024e9e5.chunk.js"
+    "revision": "a9054b62e51954d77f37",
+    "url": "/react-fancy-timeline/static/js/2.e8e83157.chunk.js"
   },
   {
     "revision": "9b318b6fb13190fe82c0677e9264b3c7",
-    "url": "/react-fancy-timeline/static/js/2.f024e9e5.chunk.js.LICENSE.txt"
+    "url": "/react-fancy-timeline/static/js/2.e8e83157.chunk.js.LICENSE.txt"
   },
   {
-    "revision": "c706e1f83e6f64242e82",
-    "url": "/react-fancy-timeline/static/js/main.7e11417b.chunk.js"
+    "revision": "d3e04443ea38635c93b1",
+    "url": "/react-fancy-timeline/static/js/main.eecaea7a.chunk.js"
   },
   {
     "revision": "c2f2862ed1e9ba5ce482",
