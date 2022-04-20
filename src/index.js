6b4b6c3c6214542036d2f994054d5bf14ac62f94
Pavel Antonov <freislot@gmail.com>
2022-04-20 03:53:28 +0300

    Fix ordering

--- src/index.js
+++ src/index.js
@@ -41,7 +41,7 @@ export const Timeline = ({ data = [], lightTheme = false }) => {
           endDate: { year: Number(x?.endDate.split('.').at(1)), month: Number(x?.endDate.split('.').at(0)) }
         }),
         startMonth: Number(x?.startDate.split('.').at(0)),
-        order: String(x?.order) || data?.indexOf(x)
+        order: x?.order || data?.indexOf(x)
       }));
     // записываем в массив с годами
     yearsArray.push({
